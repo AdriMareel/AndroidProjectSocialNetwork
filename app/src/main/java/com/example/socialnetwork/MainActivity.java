6da232053da6964b50dc08514d05f4ab1afd2cc7
Adrien Mareel <mareel.adri@yahoo.com>
2022-11-04 16:31:46 +0100

    push db data user

--- app/src/main/java/com/example/socialnetwork/MainActivity.java
+++ app/src/main/java/com/example/socialnetwork/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.socialnetwork;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText username,password;
     Button submit;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         username = (EditText) findViewById(R.id.editTextUsername);
         password = (EditText) findViewById(R.id.editTextPassword);
+
+
+
 
         //Listener sur le bouton submit
         submit = findViewById(R.id.submitButton);
@@ -29,6 +47,26 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(username.getText().toString());
                     System.out.println(password.getText().toString());
                     System.out.println("------------------------");
+
+                    //push data into database
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("password", password.getText().toString());
+                    data.put("username",username.getText().toString());
+
+                    db.collection("users").add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+
+                    System.out.println("********* PUSH DATABASE *************");
 
                     //switching activity
                     openHomeActivity();
