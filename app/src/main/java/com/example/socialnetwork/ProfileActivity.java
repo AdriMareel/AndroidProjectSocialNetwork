936c424cd9ded2150dfe54e6fd1de022c212f4e4
HyouZ <lucas.zhou@student.yncrea.fr>
2022-11-17 19:30:35 +0100

    AJOUT RETROFIT
    Modification du profileActivity + remove BIO

--- app/src/main/java/com/example/socialnetwork/ProfileActivity.java
+++ app/src/main/java/com/example/socialnetwork/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.example.socialnetwork;
 
+import static java.lang.Math.round;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,6 +11,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,6 +24,12 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ProfileActivity extends AppCompatActivity {
 
     String username;
@@ -28,10 +38,28 @@ public class ProfileActivity extends AppCompatActivity {
     MyAdapter myAdapter;
     ArrayList<Post> list;
 
+    int KTC = (int) 273.15;
+
+    public static String BaseUrl = "https://api.openweathermap.org/";
+    public static String AppId = "da4f60186a7a5cacdbffe1153ac1d6eb";
+    public static String lat = "50.6088047";
+    public static String lon = "3.0357831";
+
+    private TextView weatherData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        weatherData = findViewById(R.id.textView);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentData();
+            }
+        });
 
         //get profile's username
         Bundle extras = getIntent().getExtras();
@@ -51,28 +79,28 @@ public class ProfileActivity extends AppCompatActivity {
                 .whereEqualTo("Creator Username", username)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        System.out.println("ON EST LA OUAIS CA MARCHE 2");
-                        System.out.println(document.getData());
-                        Post post = new Post();
-                        post.postUsername = (String) document.getData().get("Creator Username");
-                        post.postContent = (String) document.getData().get("description");
-                        post.postTitle = (String) document.getData().get("title");
-                        post.postDate = (String) document.getData().get("Date of post");
-                        list.add(post);
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println("ON EST LA OUAIS CA MARCHE 2");
+                                System.out.println(document.getData());
+                                Post post = new Post();
+                                post.postUsername = (String) document.getData().get("Creator Username");
+                                post.postContent = (String) document.getData().get("description");
+                                post.postTitle = (String) document.getData().get("title");
+                                post.postDate = (String) document.getData().get("Date of post");
+                                list.add(post);
 
-                        System.out.println(post.postTitle);
+                                System.out.println(post.postTitle);
+                            }
+                        }
+                        myAdapter.notifyDataSetChanged();
                     }
-                }
-                myAdapter.notifyDataSetChanged();
-            }
-        });
+                });
 
         // Initialize and assign variable
-        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         // Set Home selected
         bottomNavigationView.setSelectedItemId(R.id.profile);
@@ -83,26 +111,25 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch(item.getItemId())
-                {
+                switch (item.getItemId()) {
                     case R.id.create:
-                        startActivity(new Intent(getApplicationContext(),PostActivity.class).putExtra("username",username));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), PostActivity.class).putExtra("username", username));
+                        overridePendingTransition(0, 0);
                         finish();
                         return true;
                     case R.id.feed:
-                        startActivity(new Intent(getApplicationContext(),FeedActivity.class).putExtra("username",username));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), FeedActivity.class).putExtra("username", username));
+                        overridePendingTransition(0, 0);
                         finish();
                         return true;
                     case R.id.search:
-                        startActivity(new Intent(getApplicationContext(),SearchActivity.class).putExtra("username",username));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), SearchActivity.class).putExtra("username", username));
+                        overridePendingTransition(0, 0);
                         finish();
                         return true;
                     case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class).putExtra("username",username));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class).putExtra("username", username));
+                        overridePendingTransition(0, 0);
                         finish();
                         return true;
                 }
@@ -111,4 +138,46 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
+    void getCurrentData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        WeatherService service = retrofit.create(WeatherService.class);
+        Call<WeatherResponse> call = service.getCurrentWeatherData(lat, lon, AppId);
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<WeatherResponse> call, @NonNull Response<WeatherResponse> response) {
+                if (response.code() == 200) {
+                    WeatherResponse weatherResponse = response.body();
+                    assert weatherResponse != null;
+
+                    String stringBuilder = "Country: " +
+                            weatherResponse.sys.country +
+                            "\n" +
+                            "Temperature: " +
+                            round(weatherResponse.main.temp - KTC) +
+                            "\n" +
+                            "Temperature(Min): " +
+                            round(weatherResponse.main.temp_min - KTC)  +
+                            "\n" +
+                            "Temperature(Max): " +
+                            round(weatherResponse.main.temp_max - KTC) +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main.pressure;
+
+                    weatherData.setText(stringBuilder);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<WeatherResponse> call, @NonNull Throwable t) {
+                weatherData.setText(t.getMessage());
+            }
+        });
+    }
 }
